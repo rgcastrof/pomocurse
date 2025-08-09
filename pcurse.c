935188a561e2ef3a4e95cad8e4e5f6bdec8d4961
rgcastrof <rogeriogirao1@proton.me>
2025-08-08 22:56:42 -0300

    create helper functions

--- pcurse.c
+++ pcurse.c
@@ -1,5 +1,5 @@
 #include <ncurses.h>
-#include <stdio.h>
+#include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
@@ -11,12 +11,16 @@ typedef struct Timer {
 	int seconds;
 	int session;
 	int status;
+	char timebuf[32];
+	char sessionbuf[32];
 }
 Timer;
 
 static Timer *create_timer(void);
+static void clear_screen(void);
 static void draw_menu(int choice);
 static int select_choice(int num_opts);
+static void display_time(Timer *t, int sessioncount);
 static void run(Timer *t);
 static void show_info(void);
 // static void set_time(Timer *t);
@@ -43,9 +47,16 @@ static Timer
 }
 
 static void
+clear_screen(void)
+{
+	clear();
+	mvprintw(BEGIN_ROW - 2, (COLS - strlen(title)) / 2, "%s", title);
+}
+
+static void
 draw_menu(int choice)
 {
-	mvprintw(BEGIN_ROW - 2, (COLS - strlen(title)) / 2, "%s", title);
+	clear_screen();
 	for (int i = 0; i < num_opts; i++) {
 		if (i == choice) {
 			mvprintw(BEGIN_ROW + i, ((COLS - strlen(opts[i])) / 2) - 2, "> %s", opts[i]);
@@ -80,13 +91,23 @@ select_choice(int num_opts)
 }
 
 static void
+display_time(Timer *t, int sessioncount)
+{
+	snprintf(t->timebuf, sizeof(t->timebuf), "Time: %02d:%02d", t->minutes, t->seconds);
+	mvprintw(BEGIN_ROW, (COLS - strlen("SESSION: POMODORO")) / 2, "SESSION: POMODORO");
+	mvprintw(BEGIN_ROW + 1, (COLS - strlen(t->timebuf)) / 2, "%s", t->timebuf);
+
+	snprintf(t->sessionbuf, sizeof(t->sessionbuf), "%d/%d", sessioncount, t->session);
+	mvprintw(BEGIN_ROW + 3, (COLS - strlen(t->sessionbuf)) / 2, "%s", t->sessionbuf);
+	refresh();
+}
+
+static void
 run(Timer *t)
 {
-	clear();
-	int session_count = 1;
-	char time[32];
-	char session[32];
-	while (session_count != t->session + 1) {
+	clear_screen();
+	int sessioncount = 1;
+	while (sessioncount != t->session + 1) {
 		if (t->seconds < 0) {
 			t->seconds = 59;
 			t->minutes--;
@@ -94,17 +115,8 @@ run(Timer *t)
 				t->status = 0;
 			}
 		}
-		if (t->status) {
-			mvprintw(BEGIN_ROW - 2, (COLS - strlen(title)) / 2, "%s", title);
-			snprintf(time, sizeof(time), "Time: %02d:%02d", t->minutes, t->seconds);
-			mvprintw(BEGIN_ROW, (COLS - strlen("SESSION: POMODORO")) / 2, "SESSION: POMODORO");
-			mvprintw(BEGIN_ROW + 1, (COLS - strlen(time)) / 2, "%s", time);
-		}
-
-		snprintf(session, sizeof(session), "%d/%d", session_count, t->session);
-		mvprintw(BEGIN_ROW + 3, (COLS - strlen(session)) / 2, "%s", session);
+		display_time(t, sessioncount);
 		t->seconds--;
-		refresh();
 		sleep(1);
 	}
 }
@@ -147,7 +159,6 @@ setup(void)
 	int running = 1;
 
 	while (running) {
-		clear();
 		int choice = select_choice(num_opts);
 		switch (choice) {
 			case 0:
